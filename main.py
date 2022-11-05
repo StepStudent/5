bd6dee4de937681b12925d15474a8cfb04a6e84c
Sasha Kosyakova <116301676+StepStudent@users.noreply.github.com>
2022-11-05 14:23:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+class Grandparents:
+    speed = 1
+    sick = 'coronary heart disease'
+    height = 150
+class Parents:
+    height = 165
+    speed = 5
+class Child(Parents):
+    speed = 8
+    def __init__(self):
+        print(self.sick)
+        print(self.height)
+        print(self.speed)
+max_roma_pavlo = Grandparents()
